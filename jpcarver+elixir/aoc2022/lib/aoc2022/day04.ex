322ac6e3926e5dfb0c74fa0b8c3f913775fd2d8e
Patrick Carver <patrickcarver@gmail.com>
2022-12-06 18:11:09 -0600

    Day 06 Elixir

--- jpcarver+elixir/aoc2022/lib/aoc2022/day04.ex
+++ jpcarver+elixir/aoc2022/lib/aoc2022/day04.ex
@@ -1,33 +1,21 @@
 defmodule Aoc2022.Day04 do
-  def part1(input_file \\ "day04.txt") do
+  def part1(input_file \\ "day04.txt"), do:
+    run(input_file, &is_one_inside_the_other/1)
+
+  def part2(input_file \\ "day04.txt"), do:
+    run(input_file, &does_intersect/1)
+
+  def run(input_file, fun) do
     "priv/" <> input_file
     |> File.stream!()
     |> Stream.map(&parse/1)
     |> Enum.reduce(0, fn pairs, acc ->
-      if is_one_inside_the_other(pairs), do: acc + 1, else: acc
+      if fun.(pairs), do: acc + 1, else: acc
     end)
   end
 
-  def part2(input_file \\ "day04.txt") do
-    "priv/" <> input_file
-    |> File.stream!()
-    |> Stream.map(&parse/1)
-    |> Enum.reduce(0, fn pairs, acc ->
-      if does_intersect(pairs), do: acc + 1, else: acc
-    end)
-  end
-
-  def is_one_inside_the_other([first_pair, second_pair]) do
-    first_inside_second = is_inside(first_pair, second_pair)
-    second_inside_first = is_inside(second_pair, first_pair)
-
-    case {first_inside_second, second_inside_first} do
-      {true, false} -> true
-      {false, true} -> true
-      {true, true} -> true
-      {false, false} -> false
-    end
-  end
+  def is_one_inside_the_other([first_pair, second_pair]), do:
+    is_inside(first_pair, second_pair) || is_inside(second_pair, first_pair)
 
   def is_inside([start1, finish1], [start2, finish2])
     when start1 >= start2 and finish1 <= finish2, do: true
